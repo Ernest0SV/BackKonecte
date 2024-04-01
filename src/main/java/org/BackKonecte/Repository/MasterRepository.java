fef8e2f77484243bfc94ac7445b06b6e51de1f08
joel <joelsolis26.aj@gmail.com>
2024-04-01 08:32:53 -0600

    se agrega el repositorio

---
+++ src/main/java/org/BackKonecte/Repository/MasterRepository.java
@@ -0,0 +1,12 @@
+package org.BackKonecte.Repository;
+
+import java.util.Optional;
+
+import org.konecte.BackKonecte.model.MasterModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MasterRepository extends JpaRepository <MasterModel, Long>  {
+	Optional<MasterModel> findByEmail(String email);
+}
---
+++ src/main/java/org/BackKonecte/Repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package org.BackKonecte.Repository;
+
+import java.util.Optional;
+
+import org.konecte.BackKonecte.model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <UsuarioModel, Long>  {
+	Optional<UsuarioModel> findByEmail(String email);
+}
