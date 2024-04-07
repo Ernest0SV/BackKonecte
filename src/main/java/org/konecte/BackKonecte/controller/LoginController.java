7c44b9404f0a9cc93cd43d8dd10d957e381c47b7
Abraham <macross777999@gmail.com>
2024-04-07 13:17:46 -0600

    Se modifico el LoginController

--- src/main/java/org/konecte/BackKonecte/controller/LoginController.java
+++ src/main/java/org/konecte/BackKonecte/controller/LoginController.java
@@ -2,6 +2,7 @@ package org.konecte.BackKonecte.controller;
 
 import javax.servlet.ServletException;
 
+import org.konecte.BackKonecte.config.JwtFilter;
 import org.konecte.BackKonecte.dto.Token;
 import org.konecte.BackKonecte.model.UsuarioModel;
 import org.konecte.BackKonecte.service.UsuarioService;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Calendar;
+import java.util.Date;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 @RequestMapping(path="/api/login/")
@@ -30,5 +35,17 @@ public class LoginController {
 		}//if
 		throw new ServletException("Nombre de usuario o contraseña incorrectos " + usuario.getCorreoUsuario());
 	}//loginUser
+	
+	private String generateToken(String username) {
+		Calendar calendar = Calendar.getInstance();//Fecha hora actual
+		calendar.add(Calendar.HOUR, 10); // Pruebas
+		//calendar.add(Calendar.MINUTE, 30);// Producción
+		return Jwts.builder().setSubject(username).claim("role", "user")
+		.setIssuedAt(new Date())
+		.setExpiration(calendar.getTime())
+		.signWith(SignatureAlgorithm.HS256, JwtFilter.secret)
+		.compact();
+
+	}//generateToken
 	
 }//class LoginController
